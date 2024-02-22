61e618bef7ab77f3bf8cc3d51154f215f2381d18
Keila Fernandes <keiladaianefernandes24@gmail.com>
2024-02-22 15:50:47 -0300

    fix: lint all the files

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -1,24 +1,29 @@
-
-describe('Home Component', () => {
+describe("Home Component", () => {
   beforeEach(() => {
-    cy.visit('/');
-  });
-
-  describe('Search number', () => {
-    it('should allow inputting numbers', () => {
-      cy.get('[data-test-id="search-input"]').type('5').should('have.value', '5');
-    });
-    
-    it('should find the number and highlight it when searching', () => {
-      cy.get('[data-test-id="search-input"]').type('5');
-      cy.get('[data-test-id="search-button"]').click();
-      cy.get('[data-test-id="result-card"]').should('have.class', 'bg-green-400').should('contain', '5');
-    });
-    
-    it('should show a different color if the number is not found', () => {
-      cy.get('[data-test-id="search-input"]').type('8');
-      cy.get('[data-test-id="search-button"]').click();
-      cy.get('[data-test-id="result-card"]').should('have.class', 'bg-red-400').should('contain', '-1');
-    });
+    cy.visit("/")
   })
-});
+
+  describe("Search number", () => {
+    it("should allow inputting numbers", () => {
+      cy.get('[data-test-id="search-input"]')
+        .type("5")
+        .should("have.value", "5")
+    })
+
+    it("should find the number and highlight it when searching", () => {
+      cy.get('[data-test-id="search-input"]').type("5")
+      cy.get('[data-test-id="search-button"]').click()
+      cy.get('[data-test-id="result-card"]')
+        .should("have.class", "bg-green-400")
+        .should("contain", "5")
+    })
+
+    it("should show a different color if the number is not found", () => {
+      cy.get('[data-test-id="search-input"]').type("8")
+      cy.get('[data-test-id="search-button"]').click()
+      cy.get('[data-test-id="result-card"]')
+        .should("have.class", "bg-red-400")
+        .should("contain", "-1")
+    })
+  })
+})
--- src/components/input-with-button/index.tsx
+++ src/components/input-with-button/index.tsx
@@ -1,5 +1,5 @@
 type InputWithButtonProps = {
-  buttonLabel: string,
+  buttonLabel: string
   onChange: (ecvt: React.ChangeEvent<HTMLInputElement>) => void
   OnPressButton: React.MouseEventHandler<HTMLButtonElement>
   type: React.HTMLInputTypeAttribute | undefined
@@ -7,25 +7,25 @@ type InputWithButtonProps = {
   disableButton: boolean
 }
 
-const InputWithButton = ({ 
-  buttonLabel, 
-  type, 
-  onChange, 
-  OnPressButton, 
+const InputWithButton = ({
+  buttonLabel,
+  type,
+  onChange,
+  OnPressButton,
   onPressKey,
   disableButton
 }: InputWithButtonProps) => {
   return (
     <div className="group w-72 md:w-80 lg:w-96">
       <div className="relative flex items-center">
-        <input 
+        <input
           type={type}
-          className="peer relative h-10 w-full rounded-md bg-gray-50 pl-4 pr-20 font-thin outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:drop-shadow-lg" 
+          className="peer relative h-10 w-full rounded-md bg-gray-50 pl-4 pr-20 font-thin outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:drop-shadow-lg"
           onChange={(evt) => onChange(evt)}
           onKeyDown={onPressKey}
           data-test-id={`${buttonLabel.toLowerCase()}-input`}
         />
-        <button 
+        <button
           className="absolute right-2 h-7 w-16 rounded-md bg-blue-200 text-xs font-semibold text-white transition-all duration-200 ease-in-out group-focus-within:bg-blue-400 group-focus-within:hover:bg-blue-600"
           onClick={OnPressButton}
           disabled={disableButton}

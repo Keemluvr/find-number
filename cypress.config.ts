6c551415317478b073ae6ee42636a7212315c2f8
Keila Fernandes <keiladaianefernandes24@gmail.com>
2024-02-22 15:49:21 -0300

    fix: lint code

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
     retries: {
-      runMode: 3,
+      runMode: 3
     },
     viewportHeight: 1080,
     viewportWidth: 1920,
     video: false,
-    screenshotOnRunFailure: false,
-  },
-});
+    screenshotOnRunFailure: false
+  }
+})
